e9987dfbc4df97296a3746bba44a1dca7d9f0f24
Darth9 <reyprimerjefe@gmail.com>
2024-09-17 15:36:57 +0100

    Add the argument required to repair errors

--- partition.cpp
+++ partition.cpp
@@ -2031,7 +2031,7 @@ bool TWPartition::Repair() {
 			return false;
 		gui_msg(Msg("repairing_using=Repairing {1} using {2}...")(Display_Name)("fsck.f2fs"));
 		Find_Actual_Block_Device();
-		command = "/system/bin/fsck.f2fs " + Actual_Block_Device;
+		command = "/system/bin/fsck.f2fs -y " + Actual_Block_Device;
 		LOGINFO("Repair command: %s\n", command.c_str());
 		// try to unbind /sdcard if it is still bind-mounted
 		#ifdef OF_UNBIND_SDCARD_F2FS
